6e1f6e111ada014fe24e72c3aa8e0142ba9ad13f
Gabriel Ackman <37252690+gaackman@users.noreply.github.com>
2018-04-04 21:34:07 -0400

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -1,13 +1,14 @@
+#! python3
 # inventory.py
 import sys
 bigBag = {'rope': 1, 'torch': 6, 'dagger': 1, 'arrow': 12}
-smallBag = {'silver coin': 50, 'gold coin': 42, 'bronze coin': 60}
+smallBag = {'silver coin': 50, 'gold coin': 42, 'bronze coin': 60}  #the two bags
 bigBagLatch = 1
-smallBagLatch = 1
+smallBagLatch = 1 #i wanted to indicate one was unopened
 def displayInventory(inventory):
-    if bagChoice == 'Big Bag':
+    if bagChoice == 'big bag':
         bagName = 'Big Bag'
-    if bagChoice == 'Small Bag':
+    if bagChoice == 'small bag':
         bagName = 'Small Bag'
     print('Inventory of ' + str(bagName) + ': ')
     item_total = 0
@@ -20,22 +21,22 @@ def displayInventory(inventory):
     print('Total number of items in ' + str(bagName) + ': ' + str(item_total))
 print('There are two bags, a Small Bag and a Big Bag.\nWhich bag would you like to look in to?')
 bagChoice = input()
-if bagChoice == 'Big Bag':
+if bagChoice == 'big bag':
     bigBagLatch = 0
     displayInventory(bigBag)
     print('The small bag is unopened.\n\nWould you like to open the bag? ')
-if bagChoice == 'Small Bag':
+if bagChoice == 'small bag':
     smallBagLatch = 0
     displayInventory(smallBag)
     print('The big bag is unopened.\n\nWould you like to open the bag?')
 response = input()
-if response == 'Yes' or 'yes' or 'sure':
+if response == 'yes':
     if bigBagLatch == 0:
-        bagChoice = 'Small Bag'
+        bagChoice = 'small bag'
         displayInventory(smallBag)
         print('\nBoth bags are now opened.')
     if smallBagLatch == 0:
-        bagChoice = 'Big Bag'
+        bagChoice = 'big bag'
         displayInventory(bigBag)
         print('\nBoth bags are now opened.')
 else:
